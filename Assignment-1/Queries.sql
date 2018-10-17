27c39c850a0b0e90ededc6da0bcae66cf0f45800
kiranf-webonise <43873703+kiranf-webonise@users.noreply.github.com>
2018-10-17 10:10:14 +0530

    Update Queries.sql

--- Assignment-1/Queries.sql
+++ Assignment-1/Queries.sql
@@ -1,5 +1,7 @@
 1)select name,age from emp where id IN (select emp_id from works where dept_id = 1 AND 2);
 
+3)select w.dept_id,count(w.dept_id)from works w group by w.dept_id having 200<(select sum(w1.pct_time) from works w1 where w1.dept_id=w.dept_id);
+
 4)select e.name from emp e,works w,dept d where e.id = w.dept_id and w.dept_id=d.id and e.salary >all (select max(d2.budget) from dept d2 where d2.id=d.id);
 
 5)select distinct d.manager_id  from dept d  where 200000 <all (select d2.budget from dept d2 where d2.manager_id=d.manager_id);

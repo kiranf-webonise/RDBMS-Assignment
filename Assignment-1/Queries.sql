af57bec2c063d8a237b910bdad33fc4f2c0fb6a6
kiranf-webonise <43873703+kiranf-webonise@users.noreply.github.com>
2018-10-17 08:12:00 +0530

    Update Queries.sql

--- Assignment-1/Queries.sql
+++ Assignment-1/Queries.sql
@@ -1,2 +1,8 @@
 
-select name,age from emp where id IN (select emp_id from works where dept_id = 1 AND 2);
+1)select name,age from emp where id IN (select emp_id from works where dept_id = 1 AND 2);
+
+4)select e.name from emp e,works w,dept d where e.id = w.dept_id and w.dept_id=d.id and e.salary >all (select max(d2.budget) from dept d2 where d2.id=d.id);
+
+5)select distinct d.manager_id  from dept d  where 200000 <all (select d2.budget from dept d2 where d2.manager_id=d.manager_id);
+
+6)select e.name  from emp e,works w,dept d where e.id=w.dept_id and w.dept_id=d.id and d.budget=(select max(d2.budget) from dept d2);
